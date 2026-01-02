a87127c4a17d522e90ad485007202939c6089f55
Toni500git <toni500minecraft@gmail.com>
2026-01-02 21:35:02 +0100

    dammit

--- src/main.cpp
+++ src/main.cpp
@@ -204,12 +204,12 @@ int main(int argc, char* argv[])
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
     glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
 #elif defined(__APPLE__)
-    const char* glsl_version = "#version 120";  // GLSL 120 for OpenGL 2.1
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_ANY_PROFILE);  // ANY profile, not CORE
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_FALSE);          // No forward compatibility
-    glfwWindowHint(GLFW_COCOA_CHDIR_RESOURCES, GLFW_FALSE);
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
 #else
     // GL 3.0 + GLSL 130
     const char* glsl_version = "#version 330 core";
